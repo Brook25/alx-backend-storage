dd311727dc863210c0fbf69e7efcaf874224024c
Brook Endale <bekelebrook24@gmail.com>
2023-05-09 22:15:11 +0300

    All tasks done

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Module contains function that checks logs
+'''Task 15
 '''
 from pymongo import MongoClient
 
@@ -10,9 +10,9 @@ def print_nginx_request_logs(nginx_collection):
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
+    for m in methods:
+        req_count = len(list(nginx_collection.find({'method': m})))
+        print('\tmethod {}: {}'.format(m, req_count))
     status_checks_count = len(list(
         nginx_collection.find({'method': 'GET', 'path': '/status'})
     ))
@@ -25,7 +25,9 @@ def print_top_ips(server_collection):
     print('IPs:')
     request_logs = server_collection.aggregate(
         [
-            {'$group': {'_id': "$ip", 'totalRequests': {'$sum': 1}}},
+            {
+                '$group': {'_id': "$ip", 'totalRequests': {'$sum': 1}}
+            },
             {
                 '$sort': {'totalRequests': -1}
             },
@@ -41,7 +43,7 @@ def print_top_ips(server_collection):
 
 
 def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
+    '''spits out some stats about Nginx logs stored in MongoDB.
     '''
     client = MongoClient('mongodb://127.0.0.1:27017')
     print_nginx_request_logs(client.logs.nginx)
