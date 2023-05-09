fb4ff8a2b108cd90814ab63594aed2cfbf858d77
Brook Endale <bekelebrook24@gmail.com>
2023-05-09 21:52:34 +0300

    All tasks done

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python3
-'''contains function that imporves 12-log_stats by adding
-10 of the most present ips
-'''
+"""
+Python script that provides some stats
+about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
 
-def print_nginx_request_logs(nginx_collection):
-    '''function prints stats about Nginx request logs.
-    '''
-    print('{} logs'.format(nginx_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for m in methods:
-        req_count = len(list(nginx_collection.find({'method': m})))
-        print('\tmethod {}: {}'.format(m, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
-
-
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
+    nginx_collection = client.logs.nginx
+    gets = nginx_collection.count_documents({'method': 'GET'})
+    posts = nginx_collection.count_documents({'method': 'POST'})
+    puts = nginx_collection.count_documents({'method': 'PUT'})
+    patchs = nginx_collection.count_documents({'method': 'PATCH'})
+    deletes = nginx_collection.count_documents({'method': 'DELETE'})
+    params = {'method': 'GET', 'path': '/status'}
+    status_check = nginx_collection.count_documents(params)
 
+    ips_data = nginx_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}, {"$limit": 10}
+   ])
 
-if __name__ == '__main__':
-    run()
+    print(f'{nginx_collection.count_documents({})} logs')
+    print('Methods:')
+    print(f'    method GET: {gets}')
+    print(f'    method POST: {posts}')
+    print(f'    method PUT: {puts}')
+    print(f'    method PATCH: {patchs}')
+    print(f'    method DELETE: {deletes}')
+    print(f'{status_check} status check')
+    print('IPs:')
+    for ip in ips_data:
+        print(f"    {ip.get('_id')}: {ip.get('count')}")
