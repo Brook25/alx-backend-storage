1d58983cae8f39ed61ba18c3ea76c0055e053eb2
Brook Endale <bekelebrook24@gmail.com>
2023-05-09 22:27:35 +0300

    All tasks done

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,54 +1,36 @@
 #!/usr/bin/env python3
-'''Task 15
-'''
+"""
+Python script that provides some stats
+about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
 
-def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
-    print('{} logs'.format(nginx_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for m in methods:
-        req_count = len(list(nginx_collection.find({'method': m})))
-        print('\tmethod {}: {}'.format(m, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
-
-
-def print_top_ips(server_collection):
-    '''Prints statistics about the top 10 HTTP IPs in a collection.
-    '''
-    print('IPs:')
-    request_logs = server_collection.aggregate(
-        [
-            {
-                '$group': {'_id': "$ip", 'totalRequests': {'$sum': 1}}
-            },
-            {
-                '$sort': {'totalRequests': -1}
-            },
-            {
-                '$limit': 10
-            },
-        ]
-    )
-    for request_log in request_logs:
-        ip = request_log['_id']
-        ip_requests_count = request_log['totalRequests']
-        print('\t{}: {}'.format(ip, ip_requests_count))
-
-
-def run():
-    '''spits out some stats about Nginx logs stored in MongoDB.
-    '''
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
-    print_top_ips(client.logs.nginx)
+    nginx_collection = client.logs.nginx
+    gets = nginx_collection.count_documents({'method': 'GET'})
+    posts = nginx_collection.count_documents({'method': 'POST'})
+    puts = nginx_collection.count_documents({'method': 'PUT'})
+    patchs = nginx_collection.count_documents({'method': 'PATCH'})
+    deletes = nginx_collection.count_documents({'method': 'DELETE'})
+    params = {'method': 'GET', 'path': '/status'}
+    status_check = nginx_collection.count_documents(params)
 
+    ips_data = nginx_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}, {"$limit": 10}
+    ])
 
-if __name__ == '__main__':
-    run()
+    print(f'{nginx_collection.count_documents({})} logs')
+    print('Methods:')
+    print(f'\tmethod GET: {gets}')
+    print(f'\tmethod POST: {posts}')
+    print(f'\tmethod PUT: {puts}')
+    print(f'\tmethod PATCH: {patchs}')
+    print(f'\tmethod DELETE: {deletes}')
+    print(f'{status_check} status check')
+    print('IPs:')
+    for ip in ips_data:
+        print(f"\t{ip.get('_id')}: {ip.get('count')}")
+
