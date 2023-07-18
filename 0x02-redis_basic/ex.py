8d388f3adb32b044d1379897dccc2539f10639f3
Brook25 <bekelebrook24@gmail.com>
2023-07-18 17:58:34 +0000

    task 1

---
+++ 0x02-redis_basic/ex.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Redis Module"""
+import redis
+import uuid
+from typing import Union, Optional, Callable
+
+
+class Cache:
+    """Redis caching class"""
+    def __init__(self):
+        """Init method"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Method stores a given value with a key"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable]):
+        """Returns a value in the desired format"""
+        if not callable(fn):
+            raise ValueError
+        if fn and key:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str):
+        """parametrizes get() with a method that converts to str"""
+        return self.get(key, lambda b: b.decode("utf-8"))
+
+    def get_int(seld, key: str):
+        """parametrizes get() with a method that converts to int"""
+        return self.get(key, lambda b: int(b))
