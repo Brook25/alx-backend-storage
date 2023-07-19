60a32a76e0a3944bcc39393e8e875822cac4d610
Brook25 <bekelebrook24@gmail.com>
2023-07-19 13:50:10 +0000

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,17 @@
 import redis
 import uuid
 from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def count_calls(fn: Callable) -> Callable:
+    """decorator func with a wrapper to count number of call on cache.store"""
+    @wraps(fn)
+    def wrapper(self, *args, **kwargs):
+        """wrapper fn that does the counting and store the result in redisdb"""
+        self._redis.incr(wrapper.__qualname__)
+        return fn(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -12,13 +23,15 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method stores a given value with a key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, int, float, bytes]:
+    def get(self, key: str, fn: Callable = None) ->\
+            Union[str, int, float, bytes]:
         """Returns a value in the desired format"""
         if fn is not None and callable(fn) and key:
             return fn(self._redis.get(key))
