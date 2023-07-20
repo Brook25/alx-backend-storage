95776168d4f83b9e9a0ddeda5f8b37decae9bd21
Brook25 <bekelebrook24@gmail.com>
2023-07-20 10:53:11 +0000

    task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 '''Module for counting web requests
 '''
-import typing, requests
+import typing
+import requests
 from functools import wraps
 import redis
 
 
 r = redis.Redis()
 
-def count_url(fn: typing.Callable) -> typing.Callable:   
+
+def count_url(fn: typing.Callable) -> typing.Callable:
     '''decorator counts number of requests
         to a url.
     '''
-    
     @wraps(fn)
     def wrapper(*args, **kwargs):
         '''modifies get_page'''
         count = r.incr("count:{}".format(*args))
         if int(count) == 1:
-            r.expire("count:{}".format(*args), 60)
+            r.expire("count:{}".format(*args), 10)
         return fn(*args, **kwargs)
     return wrapper
+
 
 @count_url
 def get_page(url: str) -> str:
