1d1b18cab4833d9c2d14066cd8a1291e79e596ca
Brook25 <bekelebrook24@gmail.com>
2023-09-17 13:45:23 +0000

    edit

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@
 
 -- DROP VIEW need_meeting;
 CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 AND 
-	(last_meeting IS NULL OR DATEDIFF(CURDATE(), last_meeting) > 30);
+	(last_meeting IS NULL OR MONTH(CURDATE()) - MONTH(last_meeting) > 1);
